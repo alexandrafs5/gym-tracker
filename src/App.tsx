ccd3e0d66d05585d810a5acf63386355445214d0
Alexandra <alexandra051009@gmail.com>
2026-05-05 00:46:48 -0700

    Initial deploy ready version

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import HomePage from "./pages/HomePage";
 import RoutineBuilderPage from "./pages/RoutineBuilderPage";
 import ActiveWorkoutPage from "./pages/ActiveWorkoutPage";
